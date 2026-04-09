fe67ff331dcbffe505916ea9ee1c3ef7742b3ccd
gideonk-dev <gideonlkarbolo@gmail.com>
2026-04-09 10:12:40 +0300

    update the app

--- app/src/main/java/com/gideon/farasi_wellnessapp/MainActivity.kt
+++ app/src/main/java/com/gideon/farasi_wellnessapp/MainActivity.kt
@@ -7,12 +7,47 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class MainActivity : AppCompatActivity() {
+
+//    declares a variable to store our interstitial ad
+    private var mInterstitialAd: InterstitialAd?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+//        implementation of the banner add
+        MobileAds.initialize(this)
+//        Starts the Google AdMob SDK
+//        Prepares your app to load adds
+//        Must be called before showing ads
+
+        val adView=findViewById<AdView>(R.id.adView)
+//        Get the Adview from the layout
+//        Connects your Kotlin code to the ad view in xml
+//        R.id.adView-the banner ad you placed in your layout
+
+        val adRequest= AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+//        Load the Ad
+//        Send the Request to "admob"
+//        AdMob returns an ad
+//        The ad is displayed in your app
+
+
+//        call the function to load the ad
+        loadInterstitialAd()
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -31,6 +66,7 @@ class MainActivity : AppCompatActivity() {
 //            write your intents
             val recipeIntent= Intent(applicationContext, HealthyRecipies::class.java)
             startActivity(recipeIntent)
+            showInterstitialAd()
         }
 
 //        nutrition intent
@@ -40,6 +76,7 @@ class MainActivity : AppCompatActivity() {
 
             val nutritionIntent= Intent(applicationContext, NutritionAdvice::class.java)
             startActivity(nutritionIntent)
+            showInterstitialAd()
         }
 
 //        meditation intent
@@ -49,6 +86,7 @@ class MainActivity : AppCompatActivity() {
 
             val meditationIntent= Intent(applicationContext, Meditation::class.java)
             startActivity(meditationIntent)
+            showInterstitialAd()
         }
 
 //        Progress intent
@@ -91,4 +129,33 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+//    a function to load our ad from the server
+fun loadInterstitialAd() {
+    val adRequest = AdRequest.Builder().build()
+
+    InterstitialAd.load(
+        this,
+        "ca-app-pub-3940256099942544/1033173712", // Test ID
+        adRequest,
+        object : InterstitialAdLoadCallback() {
+
+            override fun onAdLoaded(ad: InterstitialAd) {
+                mInterstitialAd = ad
+            }
+
+            override fun onAdFailedToLoad(error: LoadAdError) {
+                mInterstitialAd = null
+            }
+        }
+    )
+}
+    //Show Interstitial ad
+    fun showInterstitialAd() {
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
+        }
+    }
+
+
 }
